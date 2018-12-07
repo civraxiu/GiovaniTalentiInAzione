751517664930787823fb66427c36ccd7b70c4fb0
LivioPompianu <pompianu.livio@gmail.com>
2018-12-07 17:12:55 +0100

    Minors

---
+++ Modulo 6 - JavaFX/Progetto finale/Anagrafica/src/it/ytia/anagrafica/logica/persone/Docente.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 - Nicola Atzei (atzeinicola@gmail.com)
+ * Copyright 2018 - Livio Pompianu (pompianu.livio@gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+package it.ytia.anagrafica.logica.persone;
+
+import java.util.List;
+
+
+public class Docente extends Impiegato{
+	
+	private List<String> esami;
+	
+	public Docente(
+			String nome,
+			String cognome,
+			int giorno,
+			int mese,
+			int anno,
+			int stipendio,
+			List<String> esami
+	) {
+		super(nome, cognome, giorno, mese, anno, stipendio);
+		this.esami = esami;
+	}
+	
+	@Override
+	public String toString() {
+		String output = super.toString();
+		output += "Esami: " + esami + "\n";
+		return output;
+	}
+	
+	public List<String> getEsami(){
+		return this.esami;
+	}
+}
